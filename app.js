92637fd8c65468c353f3015502dc2e8f8e0f23f7
Mushegh Andrasyan <mandrasyan@smartbet.am>
2022-01-11 18:58:33 +0400

    adds function return types

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('aassssa');
+function add(n1, n2) {
+    return n1 + n2;
+}
+function printResult(num) {
+    console.log("Result is: " + num);
+}
+printResult(add(1, 2));
---
+++ basics.ts
@@ -0,0 +1,9 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
+const a = 5;
+const b = 6;
+
+const res = add(a, b);
+console.log(res);
