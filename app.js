b7194dda0f9d5d190f76564130033a9917035869
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2022-01-17 18:15:48 +0400

    'tsconfig'

--- app.js
+++
@@ -1,7 +0,0 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-function printResult(num) {
-    console.log("Result is: " + num);
-}
-printResult(add(1, 2));
--- basics.ts
+++
@@ -1,9 +0,0 @@
-function add(a: number, b: number) {
-    return a + b;
-}
-
-const a = 5;
-const b = 6;
-
-const res = add(a, b);
-console.log(res);
